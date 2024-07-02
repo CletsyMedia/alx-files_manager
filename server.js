0b2f7d2f57895406c56b9f13ac662ebb688cbaad
CletsyMedia <Cletussam12@yahoo.com>
2024-07-02 05:52:17 +0100

    first API

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const routes = require('./routes');
+import express from 'express';
+import controllerRouting from './routes/index';
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-// Middleware to parse JSON bodies
 app.use(express.json());
-// Load all routes from routes/index.js
-app.use('/', routes);
+controllerRouting(app);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
 export default app;
