ad02b7b6a47cf8632f387965a56d28d838e68ce1
CletsyMedia <Cletussam12@yahoo.com>
2024-07-02 05:15:50 +0100

    First API

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware to parse JSON bodies
+app.use(express.json());
 // Load all routes from routes/index.js
 app.use('/', routes);
 
